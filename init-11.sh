1ca2d404be526389cdafb74d58164d4ab400c775
H3arn <75999000+H3arn@users.noreply.github.com>
2021-11-19 01:26:53 +0800

    Update init-11.sh

--- init-11.sh
+++ init-11.sh
@@ -14,8 +14,11 @@ sudo apt install nginx -y
 wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
 unzip -o trojan-go-linux-amd64.zip -d /usr/local/bin/trojan-go
 mkdir -p /usr/local/etc/trojan-go
+curl -L https://raw.githubusercontent.com/H3arn/my-vps-scripts/master/trojan-go.service -o /etc/systemd/system/trojan-go.service 
+
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s /usr/bin/zsh 
 
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
