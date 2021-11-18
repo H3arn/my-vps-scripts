18dadffc6f1f6b1fa60563dd2d5c29f63eb98986
H3arn <75999000+H3arn@users.noreply.github.com>
2021-11-18 23:35:04 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install vim curl wget unzip gnupg socat -y
+sudo apt install git vim curl wget unzip gnupg socat -y
 
 wget https://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
@@ -14,3 +14,8 @@ sudo apt install nginx -y
 wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
 unzip -o trojan-go-linux-amd64.zip -d /usr/local/bin/trojan-go
 mkdir -p /usr/local/etc/trojan-go
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
